0e3c3db91589df933374c8c3d2f5afd1d32455ef
Aaron <aronwebpro@gmail.com>
2017-10-22 22:47:17 -0500

    Base update

--- src/base.js
+++ src/base.js
@@ -1,9 +1,9 @@
 import Rebase from 're-base';
 
 const base = Rebase.createClass({
-	apiKey: 'AIzaSyDY5tq1jDwkPi9B0zqXLRLZYJKzMKipWwE',
-	authDomain: 'local-react-c28b1.firebaseapp.com',
-	databaseURL: 'https://local-react-c28b1.firebaseio.com'
+	apiKey: 'Key',
+	authDomain: 'domain',
+	databaseURL: 'url'
 });
 
 export default base;
