b5a9940ce833c8febe4eb152c8587980423cb7cd
Aaron <aronwebpro@gmail.com>
2017-10-22 21:14:31 -0500

    updates

--- src/components/mixins/_articleCard.jsx
+++ src/components/mixins/_articleCard.jsx
@@ -1,33 +1,20 @@
 import React from 'react';
 
-
-const ArticleCard = (props) => {
-	const image = props.article.img;
-	const title = props.article.title;
-	const description = props.article.description;
-	const keyPoints = props.article.key_points;
-
-	return (	
+const ArticleCard = props => {
+	return (
 		<article className="container body-content">
 			<div className="col-xs-3 col-sm-2 theme-img">
-				<img src={image} alt={title} className="img img-responsive" />
+				<img src={props.article.img} alt={props.article.title} className="img img-responsive" />
 			</div>
 			<div className="col-xs-9 col-sm-9 col-md-7 theme-text">
-				<h2>{title}</h2>
-				<p>{description}</p>
+				<h2>{props.article.title}</h2>
+				<p>{props.article.description}</p>
 			</div>
 			<div className="col-xs-12 col-xs-offset-3 col-sm-3 col-sm-offset-0 theme-highlights">
-				<ul>
-					{keyPoints.map( point => (
-						<li key={Math.random()} >{point}</li>
-					))}
-				</ul>
+				<ul>{props.article.key_points.map(point => <li key={Math.random()}>{point}</li>)}</ul>
 			</div>
 		</article>
-	)
-} 
-
-
-
+	);
+};
 
 export default ArticleCard;
--- src/index.js
+++ src/index.js
@@ -4,32 +4,28 @@ import { render } from 'react-dom';
 import { BrowserRouter, Match, Miss } from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
 
-
 //Styles
 import './css/index.css';
-
 
 //Components
 import Home from './components/Home.jsx';
 import NotFound from './components/notfound.jsx';
 import Projects from './components/Projects.jsx';
 
-
 const Root = () => {
 	return (
 		<BrowserRouter>
 			<div>
-				<Match exactly pattern="/" component={ Home } />
-				<Match exactly pattern="/projects" component={ Projects } />
-				<Miss component={ NotFound} />
+				<Match exactly pattern="/" component={Home} />
+				<Match exactly pattern="/projects" component={Projects} />
+				<Miss component={NotFound} />
 			</div>
 		</BrowserRouter>
-	)
-}
-
+	);
+};
 
 //Render the App
-render(<Root/>, document.getElementById('page'));
+render(<Root />, document.getElementById('page'));
 
 //Initiate Service worker
 registerServiceWorker();
