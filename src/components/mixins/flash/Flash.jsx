80c87d41026999a97978ef1945aef459f74a23af
Aronwebpro <aronwebpro@gmail.com>
2018-02-06 16:00:22 -0600

    flash message animation refactor

--- src/components/mixins/flash/Flash.jsx
+++ src/components/mixins/flash/Flash.jsx
@@ -1,26 +1,35 @@
 import React, {Component} from 'react';
 
-const Flash = (props) => {
-    let color;
-    switch(props.status) {
-        case 'error' :
-            color = '#c74141';
-            break;
-        case 'success' :
-            color = '#5773AD';
-            break;
-        default:
-            color = '#cecece';
+
+class Flash extends Component {
+    render() {
+        let color;
+        setTimeout(() => { 
+            this.flash.style.animation = 'none';
+            this.flash.offsetHeight;
+            this.flash.style.animation = null;
+
+        },100);
+        switch(this.props.status) {
+            case 'error' :
+                color = '#c74141';
+                break;
+            case 'success' :
+                color = '#5773AD';
+                break;
+            default:
+                color = '#cecece';
+        }
+        return (
+            <div className="flash-wrapper shake" style={ {textAlign: 'center', padding:' 2px', border:'1px solid', borderColor:color } }  ref={ (input => this.flash = input )} >
+                <div className="flash"  style={ { fontWeight: 'bold', border:'3px solid', borderColor:color } } >
+                    <p>
+                        {this.props.text}
+                    </p>
+                </div>
+            </div>    
+        )
     }
-    return (
-        <div className="flash-wrapper" style={ {textAlign: 'center', padding:' 2px', border:'1px solid', borderColor:color } } >
-            <div className="flash shake"  style={ { fontWeight: 'bold', border:'3px solid', borderColor:color } }>
-                <p>
-                    {props.text}
-                </p>
-            </div>
-        </div>    
-    )
 };
 
 export default Flash;
--- src/components/template/header/Header.jsx
+++ src/components/template/header/Header.jsx
@@ -18,8 +18,6 @@ class Header extends Component {
 			headerMobile: '70px',
 			headerMobileInner: 'scale(1)',
 			headerMobileInnerMarginTop: '0px', 
-			name: '',
-			pass: '',
 			flashMessage:'', 
 			status: '',
 			shake:''
@@ -27,21 +25,23 @@ class Header extends Component {
 	}
 	login(e) {
 		e.preventDefault();
-		const name = this.state.name;
-		let pass = false;
+		const name = this.userInput.value;
+		let pass = this.passInput.value;
+
 		//validation
 		switch(true) {
-			case this.state.name === '' && this.state.pass === '' :
+			case name === '' && pass === '' :
 				clearTimeout(this.timeOut);
 				this.setState({flashMessage: 'Username and Password can\'t be empty!', status:'error' });
 				this.timeOut = setTimeout(() => { this.setState({status:''}) }, 4000);
 				return 
-			case this.state.name === '' :
+			case name === '' :
 				clearTimeout(this.timeOut);
 				this.setState({flashMessage: 'Username can\'t be empty!', status:'error' });
 				this.timeOut = setTimeout(() => { this.setState({status:''}) }, 4000);
+				this.passInput.value = '';
 				return
-			case this.state.pass === '' :
+			case pass === '' :
 				clearTimeout(this.timeOut);
 				this.setState({flashMessage: 'Password can\'t be empty!', status:'error' });
 				this.timeOut = setTimeout(() => { this.setState({status:''}) }, 4000);
@@ -58,22 +58,25 @@ class Header extends Component {
 				this.setState({pass: '', flashMessage: res.data.error});
 				clearTimeout(this.timeOut);
 				this.timeOut = setTimeout(() => { 	this.setState({status:''}) }, 4000);
+				this.passInput.value = '';
 				return;
 			} else if(res.data.hasOwnProperty('success') && res.data.success === true ) {
-				this.setState({status: 'sucess', flashMessage: res.data.message});
+
+				this.setState({status: 'sucess', flashMessage: res.data.message, pass:''});
 				clearTimeout(this.timeOut);
 				this.timeOut = setTimeout(() => { 	this.setState({status:''}) }, 4000);
-				console.log(pass);
+				this.passInput.value = '';
+				this.userInput.value = '';
 			}
 			console.log(res);
 			this.setState({name:'', pass:''});
 			
 		})
 		.catch(err => {
-			this.setState({ flashMessage: 'Login Failed!', status:'error'});
+			this.setState({ flashMessage: 'Login Failed!', status:'error', pass:''});
 			clearTimeout(this.timeOut);
 			this.timeOut = setTimeout(() => { 	this.setState({status:''}) }, 4000);
-			
+			this.passInput.value = '';
 		});
 		
 	}
@@ -82,16 +85,18 @@ class Header extends Component {
 		let height = '0px';
 		let opacity = '0';
 		if (status == 'success') {
-			flash = (  <Flash text={message} status={status} /> );
+			flash = (  <Flash text={message} status={status} shake={this.state.shake} /> );
 			height = '80px';
 			opacity = '1';
 		} else if (status === 'error') {
-			flash = (  <Flash text={message} status={status} /> );
+			flash = (  <Flash text={message} status={status} shake={this.state.shake} /> );
 			height = '80px';
 			opacity = '1';
 		} else if (status === '') {
 			flash = undefined;
 			height = '0px';
+		} else if (status === null) {
+			flash = undefined;
 		}
 		return (
 			<div style={ {height:height, transition: 'height 0.6s, opacity 1.5s', opacity: opacity} }>
@@ -138,9 +143,9 @@ class Header extends Component {
 					{ this.flash(this.state.status, this.state.flashMessage) }
 					<form style={ {display: 'inline-block' } } onSubmit={ this.login } >
 						<label htmlFor="username">Username:</label>
-						<input name="username" type="text" onChange={ this.getUserValue } value={this.state.name} />
+						<input name="username" type="text" onSubmit={ this.getUserValue }  ref={input => this.userInput = input} />
 						<label htmlFor="">Password:</label>
-						<input name="password" type="password" onChange={ this.getPass } value={this.state.pass} />
+						<input name="password" type="password" onSubmit={ this.getPass }  ref={input => this.passInput = input} />
 						<input className="top-row_login" type="submit" onSubmit={ this.login } value="Login"/>
 						<a className="register-btn" href="/register">Register</a>
 					</form>	
